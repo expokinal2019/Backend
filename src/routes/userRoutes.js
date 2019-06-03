4728cf4d0bb061a30c0e0742994a6039437772bd
hugosmhd <47651460+hugosmhd@users.noreply.github.com>
2019-06-02 22:19:31 -0600

    Usuario con imagen

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,20 @@
 'use strict'
 
 var express = require('express');
-var userController = require('../controllers/userController');
+var UserController = require('../controllers/userController');
 var md_auth = require('../middlewares/authentication');
+
+//SUBIR IMAGEN
+var multipartyes = require('connect-multiparty');
+var md_subir = multipartyes({ uploadDir: './src/uploads/users' })
 
 var api = express.Router();
 
-api.post('/sign-up', userController.signUp);
-api.post('/login', userController.login);
-api.put('/edit-user/:id', md_auth.ensureAuth, userController.editUser);
-api.delete('/delete-user/:id', md_auth.ensureAuth, userController.deleteUser);
+api.post('/sign-up', UserController.signUp);
+api.post('/login', UserController.login);
+api.put('/edit-user/:id', md_auth.ensureAuth, UserController.editUser);
+api.delete('/delete-user/:id', md_auth.ensureAuth, UserController.deleteUser);
+api.post('/upload-image-user/:id', [md_auth.ensureAuth, md_subir], UserController.uploadImage);
+api.get('/image-user/:nameImage', UserController.getImage)
 
 module.exports = api;
