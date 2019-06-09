56334392eb5de7a14f62b45361f030357d972dd2
Rodolfo Robles <rjroblesq@gmail.com>
2019-06-09 09:31:06 -0600

    Update taskRoutes.js

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -6,9 +6,9 @@ var taskController = require('../controllers/taskController');
 
 var api = express.Router();
 
-api.post('', md_auth.ensureAuth, taskController.addTask);
-api.get('/:id', md_auth.ensureAuth, taskController.getTask);
-api.put('/:id', md_auth.ensureAuth, taskController.editTask);
-api.delete('/:id', md_auth.ensureAuth, taskController.deleteTask);
+api.post('/task', md_auth.ensureAuth, taskController.addTask);
+api.get('/task/:id', md_auth.ensureAuth, taskController.getTask);
+api.put('/task/:id', md_auth.ensureAuth, taskController.editTask);
+api.delete('/task/:id', md_auth.ensureAuth, taskController.deleteTask);
 
 module.exports = api;
