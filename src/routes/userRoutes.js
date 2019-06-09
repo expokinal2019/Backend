88516bedae9b66270ede24676e9831720675da9a
Chofito <rjroblesq@gmail.com>
2019-06-09 11:17:44 -0600

    Fix 2 userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -14,8 +14,8 @@ api.post('/sign-up', userController.signUp);
 api.post('/login', userController.login);
 api.put('/:id', md_auth.ensureAuth, userController.editUser);
 api.delete('/:id', md_auth.ensureAuth, userController.deleteUser);
-api.post('/:id/subir-imagen', [md_auth.ensureAuth, md_subir], UserController.uploadImage);
-api.get('/:nameImage', UserController.getImage);
+api.post('/:id/subir-imagen', [md_auth.ensureAuth, md_subir], userController.uploadImage);
+api.get('/:nameImage', userController.getImage);
 api.get('/test', (req, res) => res.status(200).send({message: 'test'}));
 
 module.exports = api;
