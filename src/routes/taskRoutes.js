0a9258b500ad65101d6eb99b59262f55d343cb43
Rodolfo Robles <rjroblesq@gmail.com>
2019-06-17 20:25:52 -0600

    Fix duplicated get route

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -8,7 +8,7 @@ var api = express.Router();
 
 api.post('/addTask/:idProject', md_auth.ensureAuth, taskController.addTask);
 api.get('/:id', md_auth.ensureAuth, taskController.getTask);
-api.get('/:ownerId', md_auth.ensureAuth, taskController.getTasksByOwner);
+api.get('/owner/:ownerId', md_auth.ensureAuth, taskController.getTasksByOwner);
 api.get('/:idProject/getAllTasks',md_auth.ensureAuth,taskController.getAllTasks)
 api.put('/:id', md_auth.ensureAuth, taskController.editTask);
 api.delete('/:id', md_auth.ensureAuth, taskController.deleteTask);
