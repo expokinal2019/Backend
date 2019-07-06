cdc96be9781b3175b7f5ee40d3f94174f9558117
d3h <deigoeauyonh@gmail.com>
2019-07-05 18:40:36 -0600

    Fix routes.

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -6,14 +6,15 @@ var taskController = require('../controllers/taskController');
 
 var api = express.Router();
 
-api.post('/addTask/:idProject', md_auth.ensureAuth, taskController.addTask);
+api.post('/', md_auth.ensureAuth, taskController.addTask);
+api.get('/', md_auth.ensureAuth, taskController.getTasks);
 api.get('/:id', md_auth.ensureAuth, taskController.getTask);
+api.put('/:id', md_auth.ensureAuth, taskController.editTask);
+api.delete('/:id', md_auth.ensureAuth, taskController.deleteTask);
+
 api.get('/owner/:ownerId', taskController.getTasksByOwner);
 api.get('/owner-name/:ownerName', taskController.getTasksByOwnerName);
 api.get('/:idProject/getAllTasks',md_auth.ensureAuth,taskController.getAllTasks)
-api.get('/', md_auth.ensureAuth, taskController.getTasks);
-api.put('/:id', md_auth.ensureAuth, taskController.editTask);
-api.delete('/:id', md_auth.ensureAuth, taskController.deleteTask);
 api.get('getTasksByDate/:date', md_auth.ensureAuth, taskController.getTasksByDate);
 api.get('getTasksByStatus/:status', md_auth.ensureAuth, taskController.getTasksByStatus);
 api.get('getTasksByLabels/:labels', md_auth.ensureAuth, taskController.getTasksByLabels);
